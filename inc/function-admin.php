bb6295c81e228c8f2ba0bb57d1c00d7ec8555ca4
Chris Perko <asutrane@gmail.com>
2017-08-07 22:09:52 -0500

    Lesson 2 Complete

---
+++ inc/function-admin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * Admin Page
+ */
+
+function sunset_add_admin_page()
+{
+    add_menu_page(
+        'Sunset Theme Options',
+        'Sunset',
+        'manage_options',
+        'alecaddd-sunset',
+        'sunset_theme_create_page',
+        'dashicons-admin-customizer',
+        '110');
+}
+add_action('admin_menu', 'sunset_add_admin_page');
+
+function sunset_theme_create_page() {
+    echo '<h1>Sunset Theme Options</h1>';
+
+}
