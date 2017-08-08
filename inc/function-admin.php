86487fbc57b472a181afa658707bc86e86713b8c
Chris Perko <christopher.j.perko@windstream.com>
2017-08-08 09:47:34 -0500

    Lesson 3 Complete

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -2,19 +2,45 @@
 
 
 /**
- * Add the Admin page to the backend of the WordPress theme.
+ * Add the Admin page and sub pages to the backend of the WordPress theme.
  */
 function sunset_add_admin_page()
 {
+    // Generate Sunset Admin Page
     add_menu_page(
         'Sunset Theme Options',
         'Sunset',
         'manage_options',
-        'alecaddd-sunset',
+        'alecaddd_sunset',
         function () {
-            require get_template_directory() . '/pages/admin.php';
+            require_once get_template_directory() . '/inc/templates/admin.php';
         },
         'dashicons-admin-customizer',
-        '110');
+        110);
+
+    // Generate General Sub Page
+    add_submenu_page(
+        'alecaddd_sunset',
+        'Sunset Theme Options',
+        'General',
+        'manage_options',
+        'alecaddd_sunset',
+        function () {
+            require_once get_template_directory() . '/inc/templates/admin.php';
+        }
+    );
+
+    // Generate CSS Options Sub Page
+    add_submenu_page(
+        'alecaddd_sunset',
+        'Sunset CSS Options',
+        'Custom CSS',
+        'manage_options',
+        'alecaddd_sunset_css',
+        function () {
+            require_once get_template_directory() . '/inc/templates/admin-css.php';
+        }
+    );
 }
+
 add_action('admin_menu', 'sunset_add_admin_page');
--- pages/admin.php
+++
@@ -1,1 +0,0 @@
-<h1>Sunset Theme Options</h1>
